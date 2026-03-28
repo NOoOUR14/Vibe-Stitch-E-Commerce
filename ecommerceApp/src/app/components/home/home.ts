cb53b2cb58598b179cb8c6ddcf97a175f2f4b490
Nour Jad <nour.jad1997@gmail.com>
2026-03-28 22:10:12 +0200

    Fix: set up Home component with products and routing

--- ecommerceApp/src/app/components/home/home.ts
+++ ecommerceApp/src/app/components/home/home.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ProductsComponent } from '../products/products';
+import { BestSellersComponent } from '../products/best-sellers/best-sellers';
+import { NewArrivalsComponent } from '../products/new-arrivals/new-arrivals';
+import { ExploreCollectionComponent } from '../products/explore-collection/explore-collection';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductsComponent, BestSellersComponent, NewArrivalsComponent, ExploreCollectionComponent],
+
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+
+
 
 }
